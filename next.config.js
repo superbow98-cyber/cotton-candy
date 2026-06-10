0156df351a3db838c3aec13a008c714cd94b394d
superbow98-cyber <superbow98@gmail.com>
2026-06-10 15:07:02 +0800

    Add experimental serverExternalPackages configuration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverExternalPackages: ['ffmpeg-static'],
+  },
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
   images: {
